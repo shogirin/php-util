c4b9f0101ca3ebfebadfc36277edc51ee6f9a54a
shogirin@gmail.com <shogirin@gmail.com>
2016-08-08 14:30:21 +0900

    PDO処理をwrapperで切り出し

--- model.class.php
+++ model.class.php
@@ -1,145 +1,144 @@
 <?php
+
+require_once './pdo_wrap.class.php';
+
 /*
-* DB操作
-*/
-class Model{
-	const INI_PATH = "pdo.ini";
-	protected static $conn;
-	protected $table = '';
-	protected $pk = '';
+ * DB操作
+ */
 
-	/**
-	* 一覧データ取得
-	* @param:args  検索条件
-	* 		order  並び順
-	* 		page   ページ数
-	* 		order  表示件数
-	* @return array 取得結果
-	**/
-	public function getList(
-		array $args = array(), array $order = array(),$page = 1, $limit = 20){
-		$sql = " SELECT * FROM " . $this->table . $this->__createWhereQuery($args);
-		if($order)$sql .= self::__createOrderQuery($order);
-		$limit = (int)$limit;
-		$offset = $limit * ((int)$page - 1);
-		$sql .= " LIMIT {$limit} OFFSET {$offset} ";
-		$sth = self::__bindSql($sql, $args);
-		$res = array();
-	    while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-	      $res[] = $this->__fileterOutput($row);
-	    }
-	    return $res;
-	}
+class Model {
 
-	public function getGroupCount($group, array $args = array(),
-		array $order = array(),$page = 1, $limit = 20){
-		$group = self::$conn->quote($group);
-		$sql = " SELECT {$group}, COUNT(*) FROM ". $this->table
-		. $this->__createWhereQuery($args)
-		. "GROUP BY {$group} ";
-		if($order)$sql .= self::__createOrderQuery($order);
-		$limit = (int)$limit;
-		$offset = $limit * ((int)$page - 1);
-		$sql .= " LIMIT {$limit} OFFSET {$offset} ";
-		$sth = self::__bindSql($sql, $args);
-		return $sth->fetchAll(PDO::FETCH_COLUMN);
-	}
+    protected static $conn;
+    protected $table = '';
+    protected $pk = '';
 
-	public function getCount(array $args = array()){
-		$sql = " SELECT COUNT(*) FROM " . $this->table . $this->__createWhereQuery($args);
-		$sth = self::__bindSql($sql, $args);
-		$res = $sth->fetch(PDO::FETCH_ASSOC);
-		return $res["COUNT(*)"];
-	}
+    /**
+     * 一覧データ取得
+     * @param:args  検索条件
+     * 		order  並び順
+     * 		page   ページ数
+     * 		order  表示件数
+     * @return array 取得結果
+     * */
+    public function getList(
+    array $args = array(), array $order = array(), $page = 1, $limit = 20) {
+        $sql = " SELECT * FROM " . $this->table . $this->__createWhereQuery($args);
+        if ($order)
+            $sql .= self::__createOrderQuery($order);
+        $limit = (int) $limit;
+        $offset = $limit * ((int) $page - 1);
+        $sql .= " LIMIT {$limit} OFFSET {$offset} ";
+        $sth = self::__bindSql($sql, $args);
+        $res = array();
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+            $res[] = $this->__fileterOutput($row);
+        }
+        return $res;
+    }
 
-	public function get($id){
-		$sql = "SELECT * FROM " . $this->table ." WHERE " . $this->pk . " = :". $this->pk;
-		$sth = self::__bindSql($sql, array($this->pk=>(int)$id));
-		return $this->__fileterOutput($sth->fetch(PDO::FETCH_ASSOC));
-	}
+    public function getGroupCount($group, array $args = array(), array $order = array(), $page = 1, $limit = 20) {
+        $group = self::$conn->quote($group);
+        $sql = " SELECT {$group}, COUNT(*) FROM " . $this->table
+                . $this->__createWhereQuery($args)
+                . "GROUP BY {$group} ";
+        if ($order)
+            $sql .= self::__createOrderQuery($order);
+        $limit = (int) $limit;
+        $offset = $limit * ((int) $page - 1);
+        $sql .= " LIMIT {$limit} OFFSET {$offset} ";
+        $sth = self::__bindSql($sql, $args);
+        return $sth->fetchAll(PDO::FETCH_COLUMN);
+    }
 
-	public function add(array $args){
-		$sql = "INSERT INTO " . $this->table . ' (';
-		$sql .= implode(',', array_keys($args)) . ') VALUES (';
-		$querys = array();
-		foreach ($args as $key=>$value) {
-			$querys[] = ":{$key}";
-		}
-		$sql .= implode(',', $querys) . ')';
-		$sth = self::__bindSql($sql, $this->__fileterInput($args));
-		return self::$conn->lastInsertId();
-	}
+    public function getCount(array $args = array()) {
+        $sql = " SELECT COUNT(*) FROM " . $this->table . $this->__createWhereQuery($args);
+        $sth = self::__bindSql($sql, $args);
+        $res = $sth->fetch(PDO::FETCH_ASSOC);
+        return $res["COUNT(*)"];
+    }
 
-	public function delete($id){
-		$sql = "DELETE FROM ". $this->table;
-		$sql .= " WHERE " . $this->pk . " = :" . $this->pk;
-		return self::__bindSql($sql, array($this->pk=>(int)$id));
-	}
+    public function get($id) {
+        $sql = "SELECT * FROM " . $this->table . " WHERE " . $this->pk . " = :" . $this->pk;
+        $sth = self::__bindSql($sql, array($this->pk => (int) $id));
+        return $this->__fileterOutput($sth->fetch(PDO::FETCH_ASSOC));
+    }
 
-	public function update(array $args, $id){
-		$sql = "UPDATE " . $this->table . " SET ";
-		$querys = array();
-		foreach(array_keys($args) as $key){
-			if($key != $this->pk)$querys[] = " {$key} =  :{$key}";
-		}
-		$sql .= implode(',', $querys);
-		$sql .= " WHERE " . $this->pk . " = :" . $this->pk;
-		$args[$this->pk] = $id;
-		return self::__bindSql($sql, $this->__fileterInput($args));
-	}
+    public function add(array $args) {
+        $sql = "INSERT INTO " . $this->table . ' (';
+        $sql .= implode(',', array_keys($args)) . ') VALUES (';
+        $querys = array();
+        foreach ($args as $key => $value) {
+            $querys[] = ":{$key}";
+        }
+        $sql .= implode(',', $querys) . ')';
+        $sth = self::__bindSql($sql, $this->__fileterInput($args));
+        return self::$conn->lastInsertId();
+    }
 
-	public static function getConnect(){
-		self::connect();
-		return self::$conn;
-	}
+    public function delete($id) {
+        $sql = "DELETE FROM " . $this->table;
+        $sql .= " WHERE " . $this->pk . " = :" . $this->pk;
+        return self::__bindSql($sql, array($this->pk => (int) $id));
+    }
 
-	static public function connect(){
-		if(!self::$conn){
-			$ini = parse_ini_file(self::INI_PATH, true);
-			self::$conn = new PDO($ini['dsn'], $ini['username'], $ini['password']);
-			self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-	}
+    public function update(array $args, $id) {
+        $sql = "UPDATE " . $this->table . " SET ";
+        $querys = array();
+        foreach (array_keys($args) as $key) {
+            if ($key != $this->pk)
+                $querys[] = " {$key} =  :{$key}";
+        }
+        $sql .= implode(',', $querys);
+        $sql .= " WHERE " . $this->pk . " = :" . $this->pk;
+        $args[$this->pk] = $id;
+        return self::__bindSql($sql, $this->__fileterInput($args));
+    }
 
-	protected function __bindSql($sql, $args){
-		try{
-			self::connect();
-			$sth = self::$conn->prepare($sql);
-			$this->__castTypeArgs($args);
-			foreach($args as $key => $value){
-				$sth->bindValue(":{$key}", $value, 
-					is_int($value)?PDO::PARAM_INT : PDO::PARAM_STR);
-			}
-			$sth->execute();
-			return $sth;
-		} catch (PDOException $e) {
-  			echo $e->getMessage();
-  			exit;
-		}
-		return false;
-	}
+    public static function getConnect() {
+        self::connect();
+        return self::$conn;
+    }
 
-	protected function __createOrderQuery(array $args){
-		$querys = array();
-		foreach($args as $key => $value){
-			$querys[] = self::$conn->quote(" {$key} {$value} ");
-		}
-		return " ORDER BY" . implode(',', $querys);
-	}
+    static public function connect() {
+        if (!self::$conn) {
+            self::$conn = PDOWrap::connect();
+        }
+    }
 
-	protected function __createWhereQuery(array $args){
-		return '';
-	}
+    protected function __bindSql($sql, $args) {
+        try {
+            $this->__castTypeArgs($args);
+            self::connect();
+            return self::$conn->createStatement($sql, $args);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit;
+        }
+        return false;
+    }
 
-	protected function __castTypeArgs(&$args){
-	}
+    protected function __createOrderQuery(array $args) {
+        $querys = array();
+        foreach ($args as $key => $value) {
+            $querys[] = self::$conn->quote(" {$key} {$value} ");
+        }
+        return " ORDER BY" . implode(',', $querys);
+    }
 
-	protected function __fileterOutput($data){
-		return $data;
-	}
+    protected function __createWhereQuery(array $args) {
+        return '';
+    }
 
-	protected function __fileterInput($data){
-		return $data;
-	}
+    protected function __castTypeArgs(&$args) {
+
+    }
+
+    protected function __fileterOutput($data) {
+        return $data;
+    }
+
+    protected function __fileterInput($data) {
+        return $data;
+    }
 
 }
---
+++ pdo_wrap.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class PDOWrap extends PDO {
+
+    const INI_PATH = "pdo.ini";
+
+    static private $conn;
+
+    public function __construct() {
+        $ini = parse_ini_file(self::INI_PATH, true);
+        parent::__construct($ini['dsn'], $ini['username'], $ini['password']);
+    }
+
+    static public function connect() {
+        if (!self::$conn) {
+            self::$conn = new PDOWrap();
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    public function createStatement($sql, $bind = array()) {
+        $sth = $this->prepare($sql);
+        foreach ($bind as $key => $value) {
+            $sth->bindValue(":{$key}", $value, is_null($value) ? PDO::PARAM_NULL : ( is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR));
+        }
+        $sth->execute();
+        return $sth;
+    }
+
+}
